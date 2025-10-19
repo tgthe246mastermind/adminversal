332304cea926e9cbbadce4a74c4899d1644254d7
tgthe246mastermind <137459085+tgthe246mastermind@users.noreply.github.com>
2025-10-19 00:31:54 -0400

    Delete CANVA_CLONE directory

--- CANVA_CLONE/client/src/app/subscription/cancel/page.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-function SubscriptionCancel() {
-  return (
-    <div>Some Error occured while processing payment! Please try again</div>
-  );
-}
-
-export default SubscriptionCancel;
--- CANVA_CLONE/client/src/providers/nextauth-provider.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-
-function NextAuthProvider({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
-
-export default NextAuthProvider;
